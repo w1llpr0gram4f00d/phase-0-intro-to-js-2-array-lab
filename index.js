1116c42fc0fe0d82fdd327c854cba6da72fc18dc
Marc Cavazos <marcavazos19@icloud.com>
2022-01-01 18:45:20 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    return cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+    return cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+    return cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    return cats.shift();
+}
+
+function appendCat(name) {
+    const allCat = [...cats, name];
+    return allCat;
+}
+
+function prependCat(name) {
+    const allCat = [name, ...cats];
+    return allCat;
+}
+
+function removeLastCat() {
+    const allCat = [...cats];
+    allCat.pop();
+    return allCat;
+}
+
+function removeFirstCat() {
+    const allCat = [...cats];
+    allCat.shift();
+    return allCat;
+}
